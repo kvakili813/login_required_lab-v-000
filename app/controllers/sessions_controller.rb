9555dc6f5b37125fff094822dc1cc93e5fa8f77a
Kayvan Vakili <kvakili813@gmail.com>
2018-08-27 04:56:30 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
-  def show
-    if session.include? :name
+  def new
+  end
+
+  def create
+    if !params[:name].blank?
+      session[:name] = params[:name]
+      redirect_to root_path
     else
-      redirect_to '/sessions/new'
+      redirect_to sessions_new_path
     end
+  end
+
+  def destroy
+      session.delete :name
+      redirect_to root_path
   end
 end
